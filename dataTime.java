26cdbe3aeef82e28d0e15295172a988fe7d97c36
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2024-02-02 02:16:39 +0530

    Time and Date inbuilt method

---
+++ dataTime.java
@@ -0,0 +1,10 @@
+import java.time.*;
+public class dataTime {
+    public static void main(String[] args) {
+       LocalDate dt=LocalDate.now();
+       System.out.println(dt);
+
+       LocalTime t=LocalTime.now();
+       System.out.println(t);
+    }
+}

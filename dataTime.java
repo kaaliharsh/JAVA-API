24729392bb48cc292270c4feb3a26e1d8f74b841
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2024-02-02 02:27:12 +0530

    complete date and time api

--- dataTime.java
+++ dataTime.java
@@ -11,6 +11,15 @@ public class dataTime {
        int day=dt.getDayOfMonth();
        int month=dt.getDayOfMonth();
        System.out.println(dt);
-       
+
+
+       LocalTime time=LocalTime.now();
+        int hour=time.getHour();
+        int minute=time.getMinute();
+        int sec=time.getSecond();
+        int ns=time.getNano();
+
+        System.out.println(hour+":"+minute+":"+sec+":"+ns+":");
+
     }
 }

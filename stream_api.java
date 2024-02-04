1e1b6139cafa1c0d3a5043a0ec92fce1f7835159
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2024-02-04 15:46:58 +0530

    more info

--- stream_api.java
+++ stream_api.java
@@ -3,10 +3,16 @@ import java.util.*;
 import java.util.stream.*;
 public class stream_api {
     public static void main(String[] args) {
-        List<Integer> ls=Arrays.asList(2,3,4,5);
+
+        List<Integer> ls=Arrays.asList(9,8,1,5);
+        
         Stream<Integer> streamData=ls.stream();        //when you want to immutate the collection data or
         streamData.forEach(n->System.out.println(n));   //   not harm the collection data then stream api is used...
         
+        //sorted the  Stream
+
+        Stream<Integer> sr=streamData.sorted();
+        sr.forEach(n->System.out.println(n));
 
     }
 }
